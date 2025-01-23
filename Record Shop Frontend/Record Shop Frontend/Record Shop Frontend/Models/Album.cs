e105c41a67b2726151cc43cd19a33df07f3edb40
Joseph Eaton <josepheaton04@outlook.com>
2025-01-23 14:43:05 +0000

    change images; add model folder and classes

---
+++ Record Shop Frontend/Record Shop Frontend/Record Shop Frontend/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿namespace record_store.Entities
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Label { get; set; } = "";
+        public DateOnly ReleaseDate { get; set; }
+        public List<Genre> Genres { get; set; } = [];
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; } = new();
+    }
+}
---
+++ Record Shop Frontend/Record Shop Frontend/Record Shop Frontend/Models/Genre.cs
@@ -0,0 +1,23 @@
+﻿namespace record_store.Entities
+{
+    public enum Genre
+    {
+        HIP_HOP,
+        FUNK,
+        SOUL,
+        POP,
+        JAZZ,
+        METAL,
+        ROCK,
+        INDIE,
+        FOLK,
+        COUNTRY,
+        ELECTRONIC,
+        RAP,
+        RHYTHM_AND_BLUES,
+        REGGAE,
+        GRUNGE,
+        PROGRESSIVE_ROCK,
+        SINGER_SONGWRITER
+    }
+}
