3a4ee79f49f8d8a2b29d38d2e6d1a1bd6fbb9e27
Joseph Eaton <josepheaton04@outlook.com>
2025-01-27 15:24:53 +0000

    add basic editing of AlbumDetails functionality

---
+++ Record Shop Frontend/Record Shop Frontend/Record Shop Frontend/Models/AlbumDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Record_Shop_Frontend.Models;
+
+public record AlbumDTO
+{
+    public string Title { get; set; }
+    public string Label { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+    public List<Genre> Genres { get; set; }
+    public int ArtistId { get; set; }
+}
